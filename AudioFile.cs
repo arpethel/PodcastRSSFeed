a339a27344bcf4453a00e200509b052d52da8004
MicroJEdi <MicroJEdi@outlook.com>
2020-04-06 01:52:03 -0400

    audio

---
+++ AudioFile.cs
@@ -0,0 +1,26 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DEVELOPERSINC.PodcastRSS
+{
+    public static class AudioFile
+    {
+        [FunctionName("AudioFile")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ExecutionContext context, ILogger log)
+        {
+            string path = context.FunctionDirectory+"/Windows95.mp3";
+            string responseMessage = "";
+            responseMessage = await File.ReadAllTextAsync(path);
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
