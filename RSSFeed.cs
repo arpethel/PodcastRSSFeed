15ac7ab926c85e7de04d374d4837842ec13bb9ea
MicroJEdi <MicoJEdi@outlook.com>
2020-04-05 20:56:21 -0400

    RSSFile

--- RSSFeed.cs
+++ RSSFeed.cs
@@ -15,17 +15,19 @@ namespace DEVELOPERSINC.PodcastRSS
         [FunctionName("RSSFeed")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+            , ExecutionContext context, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
+            
+            var path = System.IO.Path.Combine(context.FunctionDirectory, "itunes.rss");
+            string returnValue = File.ReadAllText(context.FunctionDirectory+"itunes.rss");
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
+            string responseMessage = string.IsNullOrEmpty(returnValue)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
